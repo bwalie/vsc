e2d2b8015a4e45e13e9c80af8ac83cb88c61be5c
Rachel <rachel.malikha@gmail.com>
2024-01-19 19:50:54 +0200

    Add files via upload

---
+++ This Keyword/Program.cs
@@ -0,0 +1,30 @@
+﻿//this keyword
+using System;
+class Employee
+{
+    public int id;
+    public string name;
+    public float salary;
+    
+    public Employee(int id, string name, float salary)
+    {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+    public void display()
+    {
+        Console.WriteLine(id+" "+name+" "+salary);
+     
+    }
+}
+public class TestEmployee
+{
+    public static void Main(string[] args)
+    {
+        Employee e1 = new Employee(111, "abc", 130000);
+        Employee e2 = new Employee(121, "cde", 12222);
+        e1.display();
+        e2.display();
+    }
+}

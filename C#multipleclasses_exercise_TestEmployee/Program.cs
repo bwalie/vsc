601abe30bfe298226167f9ccb13f7bdec2b32d84
Rachel <rachel.malikha@gmail.com>
2024-01-19 19:54:07 +0200

    Add files via upload

---
+++ C#multipleclasses_exercise_TestEmployee/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+public class TestEmployee
+{
+    public int id;
+    public string name;
+    public int salary;
+    public void insert(int i, string n, int s)
+    {
+        id = i;
+        salary = s;
+        name = n;
+    }
+    public void display()
+    {
+        Console.WriteLine(id+ " " + name+ " "+ salary);
+    }
+}
+public class employee
+{
+    public static void Main(string[] args)
+    {
+        TestEmployee e1= new TestEmployee();
+        TestEmployee e2= new TestEmployee();
+        TestEmployee e3 = new TestEmployee();
+        e1.insert(1001,"mei" , 1200);
+        e2.insert(1002, "bwal", 1200);
+        e3.insert(1003, "myat",  1300);
+        e1.display();
+        e2.display();
+        e3.display();
+    }
+}
---
+++ Constructor method/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public class Employee
+{
+    public int id;
+    public string name;
+    public int salary;
+
+    public Employee(int i, string n, int s)
+    {
+        id = i;
+        name = n; 
+        salary = s;
+    }
+    public void display()
+    {
+        Console.WriteLine(id+" "+name+" "+salary);
+    }
+
+}
+public class TestEmployee
+{
+    public static void Main(string[] args)
+    {
+        Employee e1=new Employee(111, "rachel",  12000);
+        Employee e2 = new Employee(121, "abi", 12999);
+        e1.display();
+        e2.display();
+    }
+}

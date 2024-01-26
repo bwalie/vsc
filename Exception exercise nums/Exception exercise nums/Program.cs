d084a8438da50ee41b0c780b1a39c75767acebf0
Rachel <rachel.malikha@gmail.com>
2024-01-26 17:30:00 +0200

    Add files via upload

---
+++ Exception exercise nums/Exception exercise nums/Program.cs
@@ -0,0 +1,37 @@
+﻿class program
+{
+
+
+    public static void Main(string[] args)
+    {
+        int n = 18;
+        trynumber(n);
+        n = 7;
+        trynumber(n);
+
+    }
+
+    public static void trynumber(int a)
+    {
+        try
+        {
+            checkEvenNumber(a);
+            Console.WriteLine(a + "is even");
+        }
+
+        catch (ArgumentException ea)
+        {
+            Console.WriteLine("Error:" + ea.GetBaseException());
+        }
+    }
+
+    public static void checkEvenNumber(int number)
+    {
+        if (number % 2 != 0)
+        {
+            throw new ArgumentException(number + "is odd");
+
+        }
+    }
+}
+

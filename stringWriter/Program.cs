3d7dd3c6d621fd5c57631e8ef6fd6ae6406a4329
Rachel <rachel.malikha@gmail.com>
2024-02-09 17:37:31 +0200

    Add files via upload

---
+++ stringWriter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string text= "Hello, Welcome to C# \n it is nice site. \n it provides technical tutorials";
+        StringBuilder sb= new StringBuilder();
+
+        StringWriter w= new StringWriter(sb);
+
+        w.WriteLine(text);
+        w.Flush();
+        w.Close();
+        StringReader reader = new StringReader(sb.ToString());
+
+        while(reader.Peek() > -1)
+        {
+            Console.WriteLine(reader.ReadLine());
+        }
+
+    }
+    
+}

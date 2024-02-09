a56a5b442ac5335ec68234350c30d5f7e813124d
Rachel <rachel.malikha@gmail.com>
2024-02-09 17:33:24 +0200

    Add files via upload

---
+++ TextRead/Program.cs
@@ -0,0 +1,5 @@
+﻿using (TextReader tr = File.OpenText("C:\\Users\\Admin\\OneDrive - Petajaveden kunta,Sivistystoimi\\Desktop\\task.txt"))
+{
+    Console.WriteLine(tr.ReadToEnd());
+    Console.WriteLine(tr.ReadLine()); 
+}
---
+++ taskcreate/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (TextWriter write = File.CreateText("C:\\Users\\Admin\\OneDrive - Petajaveden kunta,Sivistystoimi\\Desktop\\task.txt")) 
+        {
+            write.WriteLine("Hello C# Developer");
+            write.WriteLine("C# File Handling");
+        }
+        Console.WriteLine("Data written Successfully");
+    }
+}

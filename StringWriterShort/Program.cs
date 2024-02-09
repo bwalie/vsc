e88bc0597c7b645bcf166cabb9f90ad0d1c89bfb
Rachel <rachel.malikha@gmail.com>
2024-02-09 17:38:18 +0200

    Add files via upload

---
+++ StringWriterShort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Text;
+StringWriter str= new StringWriter();
+str.WriteLine("hello, this message is ready by string reader class");
+str.Close();
+
+StringReader reader = new StringReader(str.ToString());
+while(reader.Peek()>1)
+{
+    Console.WriteLine(reader.ReadLine());
+}
+
